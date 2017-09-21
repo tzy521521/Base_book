828a3cd9d581ede69a3f9efcfd28ce348bc438a3
“tzy521521 <843460950@qq.com>
2017-09-21 10:38:30 +0800

    2017.9.21

--- src/chapter29/TestMinimumSpanningTree.java
+++ src/chapter29/TestMinimumSpanningTree.java
@@ -25,14 +25,19 @@ public class TestMinimumSpanningTree {
       {11, 8, 810}, {11, 9, 1187}, {11, 10, 239}
     };
 
-    WeightedGraph<String> graph1 = 
-      new WeightedGraph<>(vertices, edges);
+    WeightedGraph<String> graph1 = new WeightedGraph<>(vertices, edges);
     WeightedGraph<String>.MST tree1 = graph1.getMinimumSpanningTree();
     System.out.println("Total weight is " + tree1.getTotalWeight());
     tree1.printTree();
 
+    //高效的判断u和v是否在T中。
+    WeightedGraph<String>.MST tree2 = graph1.getMinimumSpanningTree0(0);
+    System.out.println("Total weight is " + tree2.getTotalWeight());
+    tree2.printTree();
+
+    /*
     edges = new int[][]{
-      {0, 1, 2}, {0, 3, 8}, 
+      {0, 1, 2}, {0, 3, 8},
       {1, 0, 2}, {1, 2, 7}, {1, 3, 3},
       {2, 1, 7}, {2, 3, 4}, {2, 4, 5},
       {3, 0, 8}, {3, 1, 3}, {3, 2, 4}, {3, 4, 6},
@@ -40,9 +45,10 @@ public class TestMinimumSpanningTree {
     };
 
     WeightedGraph<Integer> graph2 = new WeightedGraph<>(edges, 5);
-    WeightedGraph<Integer>.MST tree2 = 
+    WeightedGraph<Integer>.MST tree2 =
       graph2.getMinimumSpanningTree(1);
     System.out.println("\nTotal weight is " + tree2.getTotalWeight());
     tree2.printTree();
+     */
   }
 }
--- src/chapter29/WeightedGraph.java
+++ src/chapter29/WeightedGraph.java
@@ -95,27 +95,27 @@ public class WeightedGraph<V> extends AbstractGraph<V> {
     return getMinimumSpanningTree(0);
   }
   
-  /** Get a minimum spanning tree rooted at a specified vertex */
+  /** Get a minimum spanning tree rooted at a specified vertex O(n3)*/
   public MST getMinimumSpanningTree(int startingVertex) {
     // cost[v] stores the cost by adding v to the tree
     double[] cost = new double[getSize()];
     for (int i = 0; i < cost.length; i++) {
-      cost[i] = Double.POSITIVE_INFINITY; // Initial cost 
+      cost[i] = Double.POSITIVE_INFINITY; // Initial cost 所有节点加入到T中初始代价为无穷大。
     }
-    cost[startingVertex] = 0; // Cost of source is 0
+    cost[startingVertex] = 0; // Cost of source is 0  设置给定初始节点的代价为0
 
     int[] parent = new int[getSize()]; // Parent of a vertex
     parent[startingVertex] = -1; // startingVertex is the root
     double totalWeight = 0; // Total weight of the tree thus far
 
     List<Integer> T = new ArrayList<>();
-    
+
     // Expand T
     while (T.size() < getSize()) {
-      // Find smallest cost v in V - T 
+      // Find smallest cost v in V - T
       int u = -1; // Vertex to be determined
       double currentMinCost = Double.POSITIVE_INFINITY;
-      for (int i = 0; i < getSize(); i++) {
+      for (int i = 0; i < getSize(); i++) {//T是个线性表判断u顶点是否已经在T中需要O(n)的时间。
         if (!T.contains(i) && cost[i] < currentMinCost) {
           currentMinCost = cost[i];
           u = i;
@@ -126,10 +126,10 @@ public class WeightedGraph<V> extends AbstractGraph<V> {
       totalWeight += cost[u]; // Add cost[u] to the tree
 
       // Adjust cost[v] for v that is adjacent to u and v in V - T
-      for (Edge e : neighbors.get(u)) {
+      for (Edge e : neighbors.get(u)) {////T是个线性表判断v顶点是否已经在T中需要O(n)的时间。
         if (!T.contains(e.v) && cost[e.v] > ((WeightedEdge)e).weight) {
           cost[e.v] = ((WeightedEdge)e).weight;
-          parent[e.v] = u; 
+          parent[e.v] = u;
         }
       }
     } // End of while
@@ -137,6 +137,49 @@ public class WeightedGraph<V> extends AbstractGraph<V> {
     return new MST(startingVertex, parent, T, totalWeight);
   }
 
+  /** Get a minimum spanning tree rooted at a specified vertex O(n2)*/
+  public MST getMinimumSpanningTree0(int startingVertex){
+      // cost[v] stores the cost by adding v to the tree
+      double[] cost = new double[getSize()];
+      for (int i = 0; i < cost.length; i++) {
+          cost[i] = Double.POSITIVE_INFINITY; // Initial cost 所有节点加入到T中初始代价为无穷大。
+      }
+      cost[startingVertex] = 0; // Cost of source is 0  设置给定初始节点的代价为0
+
+      int[] parent = new int[getSize()]; // Parent of a vertex
+      parent[startingVertex] = -1; // startingVertex is the root
+      boolean[] isInT=new boolean[getSize()];//高效的判断u和v是否在T中。
+      double totalWeight = 0; // Total weight of the tree thus far
+
+      List<Integer> T = new ArrayList<>();
+
+      // Expand T
+      while (T.size() < getSize()) {
+          // Find smallest cost v in V - T
+          int u = -1; // Vertex to be determined
+          double currentMinCost = Double.POSITIVE_INFINITY;
+          for (int i = 0; i < getSize(); i++) {//T是个线性表判断u顶点是否已经在T中需要O(n)的时间。
+              if (!T.contains(i) && cost[i] < currentMinCost) {
+                  currentMinCost = cost[i];
+                  u = i;
+              }
+          }
+
+          T.add(u); // Add a new vertex to T
+          isInT[u]=true;
+          totalWeight += cost[u]; // Add cost[u] to the tree
+
+          // Adjust cost[v] for v that is adjacent to u and v in V - T
+          for (Edge e : neighbors.get(u)) {////T是个线性表判断v顶点是否已经在T中需要O(n)的时间。
+              if (!isInT[e.v] && cost[e.v] > ((WeightedEdge)e).weight) {
+                  cost[e.v] = ((WeightedEdge)e).weight;
+                  parent[e.v] = u;
+              }
+          }
+      } // End of while
+
+      return new MST(startingVertex, parent, T, totalWeight);
+  }
   /** MST is an inner class in WeightedGraph */
   public class MST extends Tree {
     private double totalWeight; // Total weight of all edges in the tree
